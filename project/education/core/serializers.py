46eab6222ac6496d80df78fee3d667a89148bcce
easydush <easydush@gmail.com>
2021-05-09 17:05:02 +0300

    added lesson viewset

--- project/education/core/serializers.py
+++ project/education/core/serializers.py
@@ -4,3 +4,8 @@ from rest_framework import serializers
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
