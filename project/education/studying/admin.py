17e15d478677ea9a29ee91a4a9114e0116d790fe
easydush <easydush@gmail.com>
2021-05-09 16:53:26 +0300

    added user and course interaction to backend

--- project/education/studying/admin.py
+++ project/education/studying/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from studying.models import CourseListener, ListenerAnswer
+
+admin.site.register(CourseListener)
+admin.site.register(ListenerAnswer)
